599f4ca6528bb4a1688851d47e6901b9e9f68646
Lucas De Curtis <lucas.de-curtis@epitech.eu>
2023-06-28 18:23:12 +0900

    feat: add new functions
    Signed-off-by: Lucas De Curtis <lucas.de-curtis@epitech.eu>

--- index.ts
+++ index.ts
@@ -13,12 +13,33 @@ export function formatToCurrency(value: number): string {
   return Intl.NumberFormat("fr-FR", { style: "currency", currency: "EUR", maximumFractionDigits: 0 }).format(value);
 }
 
-export const getFullName = (options: { firstName: string | null | undefined; lastName: string }): string => {
-  if (options.firstName) {
+export function getPercent(price: number): string {
+  let value = price;
+
+  if (Number.isNaN(price)) {
+    value = 0;
+  }
+  return new Intl.NumberFormat("fr-FR", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  }).format(value / 100);
+}
+
+export function getFullName(options: {
+  firstName: string | null | undefined;
+  lastName: string | null | undefined;
+}): string {
+  if (options.firstName && options.lastName) {
     return `${options.firstName} ${options.lastName}`;
   }
-  return options.lastName;
-};
+  if (options.firstName) {
+    return options.firstName;
+  }
+  if (options.lastName) {
+    return options.lastName;
+  }
+  return "";
+}
 
 export function formatNumber(num: number, pad: number): string {
   return num.toString().padStart(pad, "0");
